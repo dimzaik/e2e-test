66cfbd812b95f200c0ec5091b84add80c5c86b7d
dimzaik <dimzaik@gmail.com>
2018-02-21 01:03:36 +0200

    Update converter.js

--- converter.js
+++ converter.js
@@ -70,4 +70,30 @@ function converter(filename) {
 	return self;
 }
 */
+
+converter('data/testdata.xls');
+function converter(testdata){
+
+  var lines=testdata.split("\n");
+
+  var result = [];
+
+  var headers=lines[0].split(",");
+
+  for(var i=1;i<lines.length;i++){
+
+	  var obj = {};
+	  var currentline=lines[i].split(",");
+
+	  for(var j=0;j<headers.length;j++){
+		  obj[headers[j]] = currentline[j];
+	  }
+
+	  result.push(obj);
+
+  }
+  
+  //return result; //JavaScript object
+  return JSON.stringify(result); //JSON
+}
 module.exports = converter;
